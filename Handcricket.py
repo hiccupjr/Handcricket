a381f313b42b8421d01cc2d30177db7a207d242e
hiccupjr <hiccy03@gmail.com>
2024-11-14 12:47:28 +0530

    handcricket.py

--- Handcricket.py
+++ Handcricket.py
@@ -4,8 +4,16 @@
 
 import pyfiglet
 from random import*
-from countdown import countdown
+import time
 
+def countdown(mins,secs=0):
+    t = (mins*60) + secs
+    while t:
+        mins, secs = divmod(t,60)
+        timer = '{:02d}:{:02d}'.format(mins,secs)
+        print(timer, end = '\r'
+        time.sleep(1)
+        t -=1
 
 #banner 
 banner = pyfiglet.figlet_format('Hand Cricket Game')
