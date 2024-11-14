edd68d8b844f8152df58583b551f7f379e7d9cea
hiccupjr <hiccy03@gmail.com>
2024-11-14 12:50:27 +0530

    Handcricket.py

--- Handcricket.py
+++ Handcricket.py
@@ -11,7 +11,7 @@ def countdown(mins,secs=0):
     while t:
         mins, secs = divmod(t,60)
         timer = '{:02d}:{:02d}'.format(mins,secs)
-        print(timer, end = '\r'
+        print(timer, end = '\r')
         time.sleep(1)
         t -=1
 
